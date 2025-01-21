a4dcc415b93e2494c0d3d52768c9d5ceeafcf377
Damien <boenapfel.damien@yahoo.fr>
2025-01-21 16:47:58 +0100

    ADD : Listes des formations -> Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\FormationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
+
+        $formations = $formationRepository->findBy([]);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'formations' => $formations,
+        ]);
+    }
+
+    #[Route('/formation/{id}', name: 'show_formation')]
+    public function show(Formation $formation): Reponse
+    {
+        return $this->render('formation/show.html.twig', [
+            'formation' => $formation
         ]);
     }
 }
