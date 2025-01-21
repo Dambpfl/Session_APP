4e4f6eb8f4f3b2f34045c59040624bb6e97918d2
Damien <boenapfel.damien@yahoo.fr>
2025-01-21 16:12:58 +0100

    ADD : Script BDD / MCD looping / preview MCD

---
+++ script_session.sql
@@ -0,0 +1,203 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour session
+CREATE DATABASE IF NOT EXISTS `session` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `session`;
+
+-- Listage de la structure de table session. categorie
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_categorie` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.categorie : ~2 rows (environ)
+REPLACE INTO `categorie` (`id`, `nom_categorie`) VALUES
+	(1, 'Bureautique'),
+	(2, 'Design');
+
+-- Listage de la structure de table session. doctrine_migration_versions
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- Listage des données de la table session.doctrine_migration_versions : ~1 rows (environ)
+REPLACE INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+	('DoctrineMigrations\\Version20250121100447', '2025-01-21 10:05:11', 180);
+
+-- Listage de la structure de table session. formateur
+CREATE TABLE IF NOT EXISTS `formateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_formateur` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom_formateur` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_formateur` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.formateur : ~1 rows (environ)
+REPLACE INTO `formateur` (`id`, `nom_formateur`, `prenom_formateur`, `email_formateur`) VALUES
+	(1, 'Jean', 'Form', 'jf@test.fr');
+
+-- Listage de la structure de table session. formation
+CREATE TABLE IF NOT EXISTS `formation` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_formation` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.formation : ~2 rows (environ)
+REPLACE INTO `formation` (`id`, `nom_formation`) VALUES
+	(1, 'Développeur web'),
+	(2, 'Bureautique');
+
+-- Listage de la structure de table session. messenger_messages
+CREATE TABLE IF NOT EXISTS `messenger_messages` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`id`),
+  KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.messenger_messages : ~0 rows (environ)
+
+-- Listage de la structure de table session. module
+CREATE TABLE IF NOT EXISTS `module` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_module` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categorie_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_C242628BCF5E72D` (`categorie_id`),
+  CONSTRAINT `FK_C242628BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.module : ~5 rows (environ)
+REPLACE INTO `module` (`id`, `nom_module`, `categorie_id`) VALUES
+	(1, 'Word', 1),
+	(2, 'Excel', 1),
+	(3, 'Photoshop', 2),
+	(4, 'Illustrator', 2),
+	(5, 'In Design', 2);
+
+-- Listage de la structure de table session. programme
+CREATE TABLE IF NOT EXISTS `programme` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `session_id` int DEFAULT NULL,
+  `module_id` int DEFAULT NULL,
+  `nb_jour` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_3DDCB9FF613FECDF` (`session_id`),
+  KEY `IDX_3DDCB9FFAFC2B591` (`module_id`),
+  CONSTRAINT `FK_3DDCB9FF613FECDF` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`),
+  CONSTRAINT `FK_3DDCB9FFAFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.programme : ~5 rows (environ)
+REPLACE INTO `programme` (`id`, `session_id`, `module_id`, `nb_jour`) VALUES
+	(1, 5, 1, 2),
+	(2, 5, 2, 2),
+	(3, 5, 3, 4),
+	(4, 5, 4, 4),
+	(5, 5, 5, 3);
+
+-- Listage de la structure de table session. session
+CREATE TABLE IF NOT EXISTS `session` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_session` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `place` int NOT NULL,
+  `date_debut` datetime NOT NULL,
+  `date_fin` datetime NOT NULL,
+  `formateur_id` int DEFAULT NULL,
+  `formation_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_D044D5D4155D8F51` (`formateur_id`),
+  KEY `IDX_D044D5D45200282E` (`formation_id`),
+  CONSTRAINT `FK_D044D5D4155D8F51` FOREIGN KEY (`formateur_id`) REFERENCES `formateur` (`id`),
+  CONSTRAINT `FK_D044D5D45200282E` FOREIGN KEY (`formation_id`) REFERENCES `formation` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.session : ~6 rows (environ)
+REPLACE INTO `session` (`id`, `nom_session`, `place`, `date_debut`, `date_fin`, `formateur_id`, `formation_id`) VALUES
+	(1, 'Initiation Comptabilité', 10, '2025-01-21 08:00:00', '2065-01-21 17:00:00', NULL, NULL),
+	(2, 'Initiation à Word et Excel', 8, '2025-02-21 08:00:00', '2025-05-21 17:00:00', NULL, NULL),
+	(3, 'Initiation infographie (PS, INDD, AI)', 10, '2025-03-21 08:00:00', '2025-05-21 17:00:00', NULL, NULL),
+	(4, 'Perfectionnement Word,Excel,Powerpoint', 6, '2025-04-21 08:00:00', '2025-08-21 17:00:00', NULL, NULL),
+	(5, 'Initiation Bureautique et infographie', 10, '2018-07-12 08:00:00', '2018-08-07 17:00:00', NULL, NULL),
+	(6, 'Initiation en PHP/SQL', 12, '2024-02-05 08:00:00', '2024-12-21 17:00:00', NULL, NULL);
+
+-- Listage de la structure de table session. session_stagiaire
+CREATE TABLE IF NOT EXISTS `session_stagiaire` (
+  `session_id` int NOT NULL,
+  `stagiaire_id` int NOT NULL,
+  PRIMARY KEY (`session_id`,`stagiaire_id`),
+  KEY `IDX_C80B23B613FECDF` (`session_id`),
+  KEY `IDX_C80B23BBBA93DD6` (`stagiaire_id`),
+  CONSTRAINT `FK_C80B23B613FECDF` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_C80B23BBBA93DD6` FOREIGN KEY (`stagiaire_id`) REFERENCES `stagiaire` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.session_stagiaire : ~1 rows (environ)
+REPLACE INTO `session_stagiaire` (`session_id`, `stagiaire_id`) VALUES
+	(5, 1);
+
+-- Listage de la structure de table session. stagiaire
+CREATE TABLE IF NOT EXISTS `stagiaire` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_stagiaire` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom_stagiaire` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sexe` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_naissance` datetime NOT NULL,
+  `ville` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telephone` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.stagiaire : ~1 rows (environ)
+REPLACE INTO `stagiaire` (`id`, `nom_stagiaire`, `prenom_stagiaire`, `sexe`, `date_naissance`, `ville`, `email`, `telephone`) VALUES
+	(1, 'Jay', 'Tudy', 'M', '1980-02-08 16:08:02', 'Strasbourg', 'Jay@test.fr', '06.08.09.08.09');
+
+-- Listage de la structure de table session. utilisateur
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_verified` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table session.utilisateur : ~1 rows (environ)
+REPLACE INTO `utilisateur` (`id`, `email`, `roles`, `password`, `is_verified`) VALUES
+	(3, 'admin@test.fr', '[]', '$2y$13$DV9LrqLJbC1csCVnNFz/AODizS5SiyNwWfQ88kATmkszyvkWr1CDO', 1);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
