a6e9050d7a620712100fc71d60876bc2c74c40aa
Damien <boenapfel.damien@yahoo.fr>
2025-01-22 21:33:50 +0100

    ADD : Formulaire d'ajout de stagiaire

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,7 +30,31 @@ final class PersonneController extends AbstractController
             'stagiaires' => $stagiaires, 
         ]);
     }
+    
+    #[Route('/stagiaire/new', name: 'new_stagiaire')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $stagiaire = new Stagiaire();
+        
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
 
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()){
+            
+            $stagiaire = $form->getData();
+            
+            $entityManager->persist($stagiaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stagiaire');
+        }
+
+        return $this->render('personne/new.html.twig', [
+            'formAddStagiaire' => $form
+        ]);        
+    }
+    
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
     public function show(Stagiaire $stagiaire): Response
     {
--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -4,29 +4,66 @@ namespace App\Form;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomStagiaire')
-            ->add('prenomStagiaire')
-            ->add('sexe')
-            ->add('dateNaissance', null, [
-                'widget' => 'single_text',
+            ->add('nomStagiaire', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
             ])
-            ->add('ville')
-            ->add('email')
-            ->add('telephone')
+            ->add('prenomStagiaire', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('sexe', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('dateNaissance', DateType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('telephone', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
             ->add('sessions', EntityType::class, [
                 'class' => Session::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nomSession',
                 'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false
+            ])
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
             ])
         ;
     }
