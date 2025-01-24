7b4289ec74be7f34d1a644609d20e319ab2e13b1
Damien <boenapfel.damien@yahoo.fr>
2025-01-24 16:55:35 +0100

    ADD : CRUD Formateur CRUD session,module ( à fix )

---
+++ src/Form/SessionType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Formateur;
+use App\Entity\Formation;
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomSession', TextType::class)
+            ->add('place', IntegerType::class)
+            ->add('dateDebut', DateType::class)
+            ->add('dateFin', DateType::class)
+
+            ->add('formateur', EntityType::class, [
+                'class' => Formateur::class,
+                'choice_label' => 'nomFormateur',
+            ])
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'nomFormation',
+            ])
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
